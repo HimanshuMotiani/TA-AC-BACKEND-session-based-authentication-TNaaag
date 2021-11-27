58995fdd6867409b49e58ee5fe82204bc62797ce
HimanshuMotiani <himanshu.motiani93@gmail.com>
2021-11-27 14:04:56 +0530

    access

--- block-BNaadp/app.js
+++ block-BNaadp/app.js
@@ -4,7 +4,11 @@ var cookieParser = require("cookie-parser");
 var app = express()
 
 app.get("/",(req,res)=>{
+   // set cookie
     res.cookie("username","himanshu")
+
+    //access cookies
+    console.log(req.cookies);
     res.send("hello")
 })
 
